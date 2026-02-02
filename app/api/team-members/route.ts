1946c064efefafe44167079582ede788d8d930fd
nam.nguyen <nam.nguyen@redex.eco>
2026-02-02 14:13:44 +0700

    fix core values

--- app/api/team-members/route.ts
+++ app/api/team-members/route.ts
@@ -80,7 +80,6 @@ export async function GET(_request: NextRequest) {
 
         if (member.avatar) {
           avatarUrl = resolveUrl(baseUrl, member.avatar.url)
-          // environment === 'development' ? `${baseUrl}${member.avatar.url}` : member.avatar.url
         }
 
         return {
