4723a8945350f646481285c56a79d0e20965d74a
Hau Pham <phaucau2311@gmail.com>
2026-02-02 11:49:05 +0700

    change the ui on the homepage folow new design

--- components/mission-and-vision.tsx
+++ components/mission-and-vision.tsx
@@ -20,32 +20,32 @@ export default function MissionAndVision() {
           className="hidden object-cover object-bottom lg:block"
           priority
         />
-        <div className="flex flex-col md:flex-row lg:flex-row h-full w-full justify-between">
+        <div className="flex h-full w-full flex-col justify-between md:flex-row lg:flex-row">
           {/* Mission - iPad: 1 cột cân đối, text trắng trên nền xanh đậm */}
-          <div className="relative flex flex-1 flex-col gap-4 justify-center pb-6 pl-8 pr-8 pt-12 md:pb-16 md:pl-10 md:pr-10 md:pt-16 lg:pb-32 lg:pl-20 lg:pr-0 lg:pt-0 bg-[#0036AF] lg:bg-transparent">
+          <div className="relative flex flex-1 flex-col justify-center gap-4 bg-[#0036AF] pb-6 pl-8 pr-8 pt-12 md:pb-16 md:pl-10 md:pr-10 md:pt-16 lg:bg-transparent lg:pb-32 lg:pl-20 lg:pr-0 lg:pt-0">
             <Image
               src="/home/mission-image-new.png"
               alt="AI and human connection"
               width={117}
               height={119}
-              className="md:w-[100px] md:h-[102px] lg:w-[117px] lg:h-[119px]"
+              className="md:h-[102px] md:w-[100px] lg:h-[119px] lg:w-[117px]"
             />
             <div className="font-[Manrope] text-[32px] font-semibold leading-[110%] tracking-normal text-white md:text-[36px] lg:text-[42px] lg:text-[#FFFFFF]">
               {t('mission')}
             </div>
-            <p className="w-auto align-middle font-[Manrope] text-[18px] font-normal leading-[150%] tracking-normal text-white md:text-[19px] lg:w-[486px] lg:text-[20px] lg:text-[#FFFFFF]">
+            <p className="w-auto pr-[50px] align-middle font-[Manrope] text-[18px] font-normal leading-[150%] tracking-normal text-white md:text-[19px] lg:w-[486px] lg:text-[20px] lg:text-[#FFFFFF]">
               {t('mission_desc')}
             </p>
           </div>
 
           {/* Vision - iPad: 1 cột cân đối, text xanh đậm trên nền xanh nhạt */}
-          <div className="relative flex flex-1 flex-col gap-4 items-end justify-center pb-20 pl-8 pr-8 pt-12 text-right md:pb-16 md:pl-10 md:pr-10 md:pt-16 lg:pb-32 lg:pl-0 lg:pr-20 lg:pt-0 bg-[#C7EFF0] lg:bg-transparent">
+          <div className="relative flex flex-1 flex-col items-end justify-center gap-4 bg-[#C7EFF0] pb-20 pl-8 pr-8 pt-12 text-right md:pb-16 md:pl-10 md:pr-10 md:pt-16 lg:bg-transparent lg:pb-32 lg:pl-0 lg:pr-20 lg:pt-0">
             <Image
               src="/home/vision-image-new.png"
               alt="AI and human connection"
               width={104}
               height={106}
-              className="md:w-[90px] md:h-[92px] lg:w-[104px] lg:h-[106px]"
+              className="md:h-[92px] md:w-[90px] lg:h-[106px] lg:w-[104px]"
             />
             <div className="font-[Manrope] text-[32px] font-semibold leading-[110%] tracking-normal text-[#0036AF] md:text-[36px] lg:text-[42px]">
               {t('vision')}
